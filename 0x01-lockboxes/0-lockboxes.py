7e610d7ec9fe4cda4422699091e57d6f01deb8ed
olaniyo <opriolani>
2023-07-31 05:39:51 -0700

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This solves the lock boxes puzzle """
+
+def canUnlockAll(boxes=[]):
+    """This is a function that returns True of all box in boxes can be opened
+    """
+    if not boxes:
+        return False
+
+    keys = set([0])
+    for box_id, box in enumerate(boxes):
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
+
+    if len(keys) != len(boxes):
+        return False
+
+    return True
+
+
+if __name__ == '__main__':
+    boxes = [
+                [1, 3],
+                [2],
+                [3, 0],
+                [1, 2, 3],
+            ]
+    print(unlockBoxes(boxes))
+
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(unlockBoxes(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(unlockBoxes(boxes)) # Output: False
